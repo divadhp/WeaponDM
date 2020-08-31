ec10710bc1043e810e899c5491dbc5be1c148935
divadhp <divadhp@correo.ugr.es>
2020-08-31 20:52:51 +0200

    WeaponDM

---
+++ app/src/main/java/com/example/deeplearning/DrawView.kt
@@ -0,0 +1,63 @@
+package com.example.deeplearning
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.util.Log
+import android.widget.ImageView
+
+internal class DrawView : ImageView {
+    constructor(context: Context?) : super(context) {}
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {}
+    constructor(context: Context?, attrs: AttributeSet?, defStyle: Int) : super(
+        context,
+        attrs,
+        defStyle
+    ) {
+    }
+
+    var reconocimientos: Array<Recognition>? = null
+    var umbral = 0.5f
+    var color = Color.RED
+
+    override fun onDraw(canvas: Canvas) {
+        Log.d("Draw", "onDraw")
+        super.onDraw(canvas)
+        val paint = Paint()
+        paint.color = color
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = 10f
+        paint.textSize = 60f
+
+        val textPaint = Paint().apply {
+            color = Color.WHITE
+            style = Paint.Style.FILL_AND_STROKE
+            strokeWidth = 2f
+            textSize = 60f
+        }
+
+        val bgPaint = Paint(paint).apply {
+            alpha = 160
+            style = Paint.Style.FILL
+        }
+
+        reconocimientos?.let {
+            for (rec in it) {
+                if (rec.score >= umbral) {
+                    rec.box?.let {
+                        Log.d("Box", it.toShortString() + rec.score)
+                        canvas.drawRect(it, paint)
+                        val cornerSize: Float =
+                            Math.min(it.width(), it.height()) / 8.0f
+                        val text = rec.prediction + " " + "%.2f".format(rec.score * 100)  + "%"
+                        canvas.drawRect(it.left, it.top + 60, it.left + bgPaint.measureText(text), it.top, bgPaint)
+                        canvas.drawText(rec.prediction +" " + "%.2f".format(rec.score * 100) + "%", it.left, it.top+60, textPaint)
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/deeplearning/ModelListFragment.kt
@@ -0,0 +1,83 @@
+package com.example.deeplearning
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.fragment.app.Fragment
+import java.io.*
+import java.util.*
+
+class ModelListFragment(files : Array<String>, preffix: String) : Fragment() {
+
+    val files = files
+    val preffix = preffix
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.list_model, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        activity!!.title = "Lista de modelos"
+        val listAdapter = ArrayAdapter<String>(context!!, R.layout.list_item, files)
+
+        val listView: ListView = view.findViewById(R.id.listview)
+        listView.adapter = listAdapter
+
+
+        listView.setOnItemClickListener { adapterView, view, i, l ->
+            Log.d("List", adapterView.getItemAtPosition(i).toString())
+            val inputStream = activity!!.assets.open(preffix + "/" + adapterView.getItemAtPosition(i).toString())
+            val file = File.createTempFile("model", "tflite")
+            org.apache.commons.io.FileUtils.copyInputStreamToFile(inputStream, file)
+            val model = adapterView.getItemAtPosition(i).toString().split(".")[0]
+            val labelsInput = activity!!.assets.open(preffix + "/" + adapterView.getItemAtPosition(i).toString().split(".")[0] + ".label")
+            val br = BufferedReader(InputStreamReader(labelsInput))
+            val labels: Vector<String> = Vector<String>()
+            var line = br.readLine()
+            while (line != null) {
+                labels.add(line)
+                line = br.readLine()
+            }
+            if (preffix == "Detection")
+                activity!!.supportFragmentManager.beginTransaction().replace(R.id.container, CameraFragment(file, labels,model)).addToBackStack(null).commit()
+            else
+                activity!!.supportFragmentManager.beginTransaction().replace(R.id.container, CameraClassifierFragment(file, labels, model)).addToBackStack(null).commit()
+        }
+    }
+
+    private fun createFileFromInputStream(inputStream: InputStream): File? {
+
+        try {
+            val f = File("new FilePath")
+            val outputStream: OutputStream = FileOutputStream(f)
+            val buffer = ByteArray(1024)
+            var length = 0
+            while (inputStream.read(buffer).also({ length = it }) > 0) {
+                outputStream.write(buffer, 0, length)
+            }
+            outputStream.close()
+            inputStream.close()
+            return f
+        } catch (e: IOException) {
+            Log.d("List", "Error")
+        }
+        return null
+    }
+
+}
